2e930c87f96b602dd1a4b3eea76eebc062e0dc0c
khadizajarin <khadiza131310@gmail.com>
2025-03-30 02:14:32 +0600

    update POST API

--- models/Posts.ts
+++ models/Posts.ts
@@ -1,15 +1,32 @@
 import mongoose, { Document, Schema } from 'mongoose';
 
 interface IPost extends Document {
+  author: string;
   title: string;
-  content: string;
-  createdAt: Date;
+  publishedDate: Date;
+  category: string;
+  likes:number;
+  views:number;
+  subcategory: string[];  // Array of strings for subcategories
+  summary: string;
+  tags: string[];  // Array of strings for tags
+  description: string;
+  images: string[];  // Array of strings to store image paths
+
 }
 
 const PostSchema = new Schema<IPost>({
+  author: { type: String, required: true },
   title: { type: String, required: true },
-  content: { type: String, required: true },
-  createdAt: { type: Date, required: true },
+  publishedDate: { type: Date, required: true },
+  category: { type: String, required: true },
+  likes:{type: Number, required:false},
+  views:{type: Number, required:false},
+  subcategory: { type: [String], required: false },  // Optional array of subcategories
+  summary: { type: String, required: true },
+  tags: { type: [String], required: false },  // Optional array of tags
+  description: { type: String, required: true },
+  images: { type: [String], required: false },  // Optional array of image paths
 });
 
 const Post = mongoose.model<IPost>('Post', PostSchema);
